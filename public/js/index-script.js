1301953b9dfbce5e84e96fa25abc2556b5887b3a
Jinhan Du <jinhan.du@qq.com>
2021-03-07 12:42:15 +0200

    Complete Scooter Section

--- public/js/index-script.js
+++ public/js/index-script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   if ($(window).width() < 1000) {
     $("#main-nav").addClass("main-nav-darkbg");
   }
-
+  setScooterData();
   setUsageSenrioData();
 
   //导航栏动画
@@ -59,5 +59,54 @@ function setUsageSenrioData() {
     let usageSenarioFullPath = usageSenarioImgPath + i + ".jpg";
     usageSenarioImgElements.eq(i - 1).attr("src", usageSenarioFullPath);
     usageSenarioTextElements.eq(i - 1).text(usageSenarioTextArray[i - 1]);
+  }
+}
+
+function setScooterData() {
+  let scootersArray = [
+    {
+      id: "0",
+      imgPath: "./public/images/scooters/OC2B/OC2B-0.jpg",
+      name: "OC2B",
+      price: "3999 SEK",
+    },
+    {
+      id: "2",
+      imgPath: "./public/images/scooters/OC2pro-G/OC2pro-G-0.jpg",
+      name: "2Pro-G",
+      price: "34999 SEK",
+    },
+    {
+      id: "3",
+      imgPath: "./public/images/scooters/OC2R/OC2R-0.jpg",
+      name: "OC2R",
+      price: "8999 SEK",
+    },
+    {
+      id: "4",
+      imgPath: "./public/images/scooters/OC3B/OC3B-0.jpg",
+      name: "OC3B",
+      price: "34999 SEK",
+    },
+    {
+      id: "5",
+      imgPath: "./public/images/scooters/OC3R/OC3R-0.jpg",
+      name: "OC3R",
+      price: "24999 SEK",
+    },
+    {
+      id: "6",
+      imgPath: "./public/images/scooters/OC4B-G/OC4B-G0.jpeg",
+      name: "OC4B-G",
+      price: "29999 SEK",
+    },
+  ];
+  let scootersImg = $(".scooter-img-container__img");
+  let scootersName = $(".scooter-name");
+  let scooterPrice = $(".scooter-price");
+  for (let i = 0; i < 6; i++) {
+    scootersImg.eq(i).attr("src", scootersArray[i].imgPath);
+    scootersName.eq(i).text(scootersArray[i].name);
+    scooterPrice.eq(i).text(scootersArray[i].price);
   }
 }
