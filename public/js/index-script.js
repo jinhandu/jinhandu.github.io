0553135218456bb7761b7b92f9fca162dd72531f
Jinhan Du <jinhan.du@qq.com>
2021-03-29 07:45:32 +0300

    Complete Dropdown Menu

--- public/js/index-script.js
+++ public/js/index-script.js
@@ -1,15 +1,85 @@
+let scootersArray = [
+  {
+    id: "0",
+    imgPath: "./public/images/scooters/OC2B/OC2B-0.jpg",
+    name: "OCECO OC2B",
+    type: "Two Wheels Electric Scooter",
+    price: "3999 SEK",
+  },
+  {
+    id: "1",
+    imgPath: "./public/images/scooters/OC2pro-G/OC2pro-G-0.jpg",
+    name: "OCECO 2Pro-G",
+    type: "Two Wheels Electric Scooter",
+    price: "34999 SEK",
+  },
+  {
+    id: "2",
+    imgPath: "./public/images/scooters/OC2R/OC2R-0.jpg",
+    name: "OCECO OC2R",
+    type: "Two Wheels Electric Scooter",
+    price: "8999 SEK",
+  },
+  {
+    id: "3",
+    imgPath: "./public/images/scooters/OC3B/OC3B-0.jpg",
+    name: "OCECO OC3B",
+    type: "Three Wheels Electric Scooter",
+    price: "18999 SEK",
+  },
+  {
+    id: "4",
+    imgPath: "./public/images/scooters/OC3R/OC3R-0.jpg",
+    name: "OCECO OC3R",
+    type: "Three Wheels Electric Scooter",
+    price: "24999 SEK",
+  },
+  {
+    id: "5",
+    imgPath: "./public/images/scooters/OC4B-G/OC4B-G-0.jpeg",
+    type: "Four Wheels Electric Scooter",
+    name: "OCECO OC4B-G",
+    price: "29999 SEK",
+  },
+];
 $(document).ready(function () {
-  //手机版取消导航栏动画
-  if ($(window).width() < 1000) {
-    $("#main-nav").addClass("main-nav-darkbg");
-  }
+  mobileDeviceKeepDarkNav();
   setScooterData();
   setUsageSenrioData();
   setVideoPlaylist();
-  $("#send-order-btn-id").on("click", function () {
-    alert("Order Send Success!");
-  });
+  setOrderSelectorData();
+  sendOrder();
 });
+
+//点击OrderNow按钮
+function sendOrder() {
+  $("#send-order-btn-id").on("click", function () {
+    alert(
+      "Order Send Success!\nOur sales manager will contact you within 24 hours :)"
+    );
+  });
+}
+
+//order-now-scooter-model
+function setOrderSelectorData() {
+  let modelSelector = $(".order-now-scooter-model").eq(0);
+  for (let scooterObj of scootersArray) {
+    let scooterModel = `<option value="${scooterObj.name}">${scooterObj.name}</option>`;
+    modelSelector.append(scooterModel);
+  }
+  let quantitySelector = $(".order-now-scooter-quantity").eq(0);
+  for (let quantitySelet = 1; quantitySelet < 10; quantitySelet++) {
+    let scooterQuantity = `<option value="${quantitySelet}">${quantitySelet}</option>`;
+    quantitySelector.append(scooterQuantity);
+  }
+}
+
+//手机版取消导航栏动画
+function mobileDeviceKeepDarkNav() {
+  if ($(window).width() < 1000) {
+    $("#main-nav").addClass("main-nav-darkbg");
+  }
+}
 
 //点击菜单后自动收回
 $(".main-nav-items").on("click", function () {
@@ -52,50 +122,6 @@ $(document).ready(function () {
 
 //加载电动车卡片数据
 function setScooterData() {
-  let scootersArray = [
-    {
-      id: "0",
-      imgPath: "./public/images/scooters/OC2B/OC2B-0.jpg",
-      name: "OCECO OC2B",
-      type: "Two Wheels Electric Scooter",
-      price: "3999 SEK",
-    },
-    {
-      id: "1",
-      imgPath: "./public/images/scooters/OC2pro-G/OC2pro-G-0.jpg",
-      name: "OCECO 2Pro-G",
-      type: "Two Wheels Electric Scooter",
-      price: "34999 SEK",
-    },
-    {
-      id: "2",
-      imgPath: "./public/images/scooters/OC2R/OC2R-0.jpg",
-      name: "OCECO OC2R",
-      type: "Two Wheels Electric Scooter",
-      price: "8999 SEK",
-    },
-    {
-      id: "3",
-      imgPath: "./public/images/scooters/OC3B/OC3B-0.jpg",
-      name: "OCECO OC3B",
-      type: "Three Wheels Electric Scooter",
-      price: "18999 SEK",
-    },
-    {
-      id: "4",
-      imgPath: "./public/images/scooters/OC3R/OC3R-0.jpg",
-      name: "OCECO OC3R",
-      type: "Three Wheels Electric Scooter",
-      price: "24999 SEK",
-    },
-    {
-      id: "5",
-      imgPath: "./public/images/scooters/OC4B-G/OC4B-G-0.jpeg",
-      type: "Four Wheels Electric Scooter",
-      name: "OCECO OC4B-G",
-      price: "29999 SEK",
-    },
-  ];
   let scootersImg = $(".scooter-img-container__img");
   let scootersName = $(".scooter-name");
   let scooterPrice = $(".scooter-price");
